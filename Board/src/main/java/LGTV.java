e28719e88f49bc26b89a76d4dff0c95c2738a2d4
이승원 <squareo073@gmail.com>
2022-01-21 17:20:53 +0900

    commit

---
+++ Board/src/main/java/LGTV.java
@@ -0,0 +1,19 @@
+public class LGTV() {
+
+    public void turnOn() {
+        System.out.println("LGTV 킨다");
+    }
+
+    public void turnOff() {
+        System.out.println("LGTV 끈다");
+    }
+
+    public void soundUp() {
+        System.out.println("LGTV 소리올림");
+    }
+
+    public void soundDown() {
+        System.out.println("LGTV 소리내림");
+    }
+
+}
---
+++ Board/src/main/java/SamsungTV.java
@@ -0,0 +1,19 @@
+public class SamsungTV {
+
+    public void powerOn(){
+        System.out.println("SSTV 킨다");
+    }
+
+    public void powerOff(){
+        System.out.println("SSTV 끈다");
+    }
+
+    public void volumeUp(){
+        System.out.println("SS소리 올림");
+    }
+
+    public void volumeDown(){
+        System.out.println("SS소리 내림");
+    }
+
+}
